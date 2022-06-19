1fd64bf6b1729eee05c05d19aa73bcb808f50fa3
leegh519 <ghkstoo@naver.com>
2022-06-20 00:59:46 +0900

    .

---
+++ baekjoon/silver2/P_11053.java
@@ -0,0 +1,26 @@
+package silver2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P_11053 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] dp = new int[n];
+		dp[0] = Integer.parseInt(st.nextToken());
+		
+		for (int i = 1; i < n; i++) {
+			
+
+			
+		}
+		
+		
+
+	}
+
+}
