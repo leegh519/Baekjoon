5ad0aa715854802219bfc7c5a55080846eb53e0a
leegh519 <ghkstoo@naver.com>
2022-06-20 17:50:39 +0900

    .

--- baekjoon/silver2/P_11053.java
+++ baekjoon/silver2/P_11053.java
@@ -11,15 +11,23 @@ public class P_11053 {
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] dp = new int[n];
-		dp[0] = Integer.parseInt(st.nextToken());
-		
-		for (int i = 1; i < n; i++) {
-			
+		int[] arr = new int[n];
 
-			
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		
+
+		dp[0] = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[j] + 1, dp[i]);
+				}
+			}
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max+1);
 
 	}
 
