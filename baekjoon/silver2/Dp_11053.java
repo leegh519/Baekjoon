07fb3cff526d69aa8d53c7bcb842a0a5255c2737
leegh519 <ghkstoo@naver.com>
2022-06-30 17:49:15 +0900

    1003번

--- baekjoon/silver2/Dp_11053.java
+++ baekjoon/silver2/Dp_11053.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P_11053 {
+public class Dp_11053 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
