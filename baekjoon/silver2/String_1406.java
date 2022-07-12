e7b80974f888168f9faff37940f7355204a142f1
leegh519 <ghkstoo@naver.com>
2022-07-12 17:53:31 +0900

    12

--- baekjoon/silver2/String_1406.java
+++ baekjoon/silver2/String_1406.java
@@ -1,8 +1,12 @@
 package silver2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 //	String 시간초과남
@@ -12,38 +16,47 @@ public class String_1406 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		sb.append(br.readLine());
+		List<Character> list = new LinkedList<>();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str = br.readLine();
+		for (int i = 0; i < str.length(); i++) {
+			list.add(str.charAt(i));
+		}
 		int m = Integer.parseInt(br.readLine());
-		int cursor = sb.length();
+		int cursor = list.size() - 1;
+
 		for (int i = 0; i < m; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
-			if (s.equals("L")) {
+			String s = br.readLine();
+			if (s.charAt(0) == 'L') {
 				if (cursor > 0) {
 					cursor--;
 				}
-			} else if (s.equals("D")) {
-				if (cursor < sb.length()) {
+			} else if (s.charAt(0) == 'D') {
+				if (cursor < list.size() - 1) {
 					cursor++;
 				}
-			} else if (s.equals("B")) {
-				if (cursor > 0) {
-					String s1 = sb.substring(0, cursor-1);
-					String s2 = sb.substring(cursor, sb.length());
-					sb.setLength(0);
-					sb.append(s1).append(s2);
-					cursor--;
+			} else if (s.charAt(0) == 'B') {
+				if (cursor >= 0) {
+					list.remove(cursor);
+					if (cursor == list.size())
+						cursor--;
 				}
-			}else if(s.equals("P")) {
-				String s1 = sb.substring(0, cursor);
-				String s2 = sb.substring(cursor, sb.length());
-				sb.setLength(0);
-				sb.append(s1).append(st.nextToken()).append(s2);
+			} else if (s.charAt(0) == 'P') {
+				list.add(cursor+1, s.charAt(2));
 				cursor++;
 			}
+			for (int j = 0; j < list.size(); j++) {
+				bw.write(list.get(j));
+			}
+			System.out.println(cursor);
+			bw.write("\n");
+			bw.flush();
 		}
-		System.out.println(sb);
+		for (int i = 0; i < list.size(); i++) {
+			bw.write(list.get(i));
+		}
+		bw.flush();
+		bw.close();
 
 	}
 
